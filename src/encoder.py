41384395ff6cf472f6319993c561f145e99d3cad
Ibrahim <ihelal@sfu.ca>
2020-05-01 00:50:47 -0700

    V1.2

---
+++ src/encoder.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import rospy
+import RPi.GPIO as GPIO
+import time
+
+from std_msgs.msg import Int64
+
+class Switch():
+    def __init__(self):
+        self.LF_ENCODER_PIN = 14
+        self.RF_ENCODER_PIN = 15
+        self.LB_ENCODER_PIN = 18
+        self.RB_ENCODER_PIN = 23
+
+        self.lf_en_val = 0
+        self.rf_en_val = 0
+        self.lb_en_val = 0
+        self.rb_en_val = 0
+
+        self.init_encoders()
+        rospy.init_node('encoder_sensor', anonymous=False)
+        self.pub_switch = rospy.Publisher('/encoder', Int64, queue_size=10)
+        
+    def init_encoders(self):
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setup(self.LF_ENCODER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.RF_ENCODER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.LB_ENCODER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.RB_ENCODER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        GPIO.add_event_detect(self.LF_ENCODER_PIN,GPIO.RISING,callback=self.lf_en_cb)
+        GPIO.add_event_detect(self.LF_ENCODER_PIN,GPIO.RISING,callback=self.rf_en_cb)
+        GPIO.add_event_detect(self.LF_ENCODER_PIN,GPIO.RISING,callback=self.lb_en_cb)
+        GPIO.add_event_detect(self.LF_ENCODER_PIN,GPIO.RISING,callback=self.rb_en_cb)
+
+    def lf_en_cb(self,channel):
+        print("")
+        self.lf_en_val += 1
+        print(self.lf_en_val)
+    
+    def rf_en_cb(self,channel):
+        print("")
+        self.rf_en_val += 1
+        print(self.lf_en_val)
+    
+    def lb_en_cb(self,channel):
+        print("")
+        self.lb_en_val += 1
+        print(self.lf_en_val)
+    
+    def rb_en_cb(self,channel):
+        print("")
+        self.rb_en_val += 1
+        print(self.lf_en_val)
+    
+    def spin(self):
+        self.rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+        GPIO.cleanup()
+    
+    def pub(self,value):
+        msg = Int64()
+        msg.data = value
+        self.pub_switch.publish(msg)
+
+if __name__ == '__main__':
+    run = Switch()
+    run.spin()
+    
---
+++ src/led.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import rospy
+from neopixel import *
+import time
+
+class led():
+    def __init__(self):
+        # LED strip configuration:
+        self.LED_COUNT      = 24      # Number of LED pixels.
+        self.LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+        #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+        self.LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+        self.LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+        self.LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+        self.LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+        self.LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+        # # Process arguments
+        # self.parser = argparse.ArgumentParser()
+        # self.parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+        # self.args = self.parser.parse_args()
+
+        # Create NeoPixel object with appropriate configuration.
+        self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, self.LED_BRIGHTNESS, self.LED_CHANNEL)
+        # Intialize the library (must be called once before other functions).
+        self.strip.begin()
+        print("gg")
+        time.sleep(1)
+        print("gg1")
+
+        rospy.init_node('xbot_led', anonymous=False)
+        rospy.Subscriber("/chatter", String, led_callback)
+
+    # Define functions which animate LEDs in various ways.
+    def colorWipe(self,strip, color, wait_ms=50):
+        """Wipe color across display a pixel at a time."""
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, color)
+            strip.show()
+            time.sleep(100/1000.0)
+            
+    def led_callback(self,data):
+        print(data)
+    
+    def spin(self):
+        self.rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+        # self.colorWipe(self.strip,Color(0,0,0))
+        # while True:
+        #     self.colorWipe(self.strip,Color(0,60,0))
+            #self.colorWipe(self.strip,Color(50,0,0))
+
+
+if __name__ == '__main__':
+    run = led()
+    run.spin()
+    
