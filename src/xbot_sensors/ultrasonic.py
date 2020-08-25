31817a0c4e7f31db5505d5fda751ab4659a5d880
Ibrahim <ihelal@sfu.ca>
2020-08-25 00:28:05 -0700

    V1.4

--- src/xbot_sensors/ultrasonic.py
+++ src/xbot_sensors/ultrasonic.py
@@ -1,32 +1,26 @@
-#!/usr/bin/python
-import RPi.GPIO as GPIO
-import time
-from mybot_sdk.robot_setup import get_robot_cfg
 
-class Ultrasonic_Sensors():
-    def __init__(self,side="ALL"):
-        Ultrasonic = get_robot_cfg()["Sensors"]["Ultrasonic"]
-        self.GPIO_TRIGGER = Ultrasonic["Trigger"]
-        self.GPIO_ECHO_F = Ultrasonic["Front"]
-        self.GPIO_ECHO_B = Ultrasonic["Back"]
-        self.GPIO_ECHO_R = Ultrasonic["Right"]
-        self.GPIO_ECHO_L = Ultrasonic["Left"]
-        self.MAX_RANGE = Ultrasonic["MaxRange"]
+class Ultrasonic_Sensors:
 
+    def __init__(self, side='ALL'):
+        Ultrasonic = get_robot_cfg()['Sensors']['Ultrasonic']
+        self.GPIO_TRIGGER = Ultrasonic['Trigger']
+        self.GPIO_ECHO_F = Ultrasonic['Front']
+        self.GPIO_ECHO_B = Ultrasonic['Back']
+        self.GPIO_ECHO_R = Ultrasonic['Right']
+        self.GPIO_ECHO_L = Ultrasonic['Left']
+        self.MAX_RANGE = Ultrasonic['MaxRange']
         self.f_us = 0
         self.b_us = 0
         self.r_us = 0
         self.l_us = 0
-
-        self.ACCURACY = 1 #Deciaml Places
+        self.ACCURACY = 2
         self.init_us()
-
         self.req_side = side
-        self.FRONT = "FRONT"
-        self.BACK  = "BACK"
-        self.RIGHT = "RIGHT"
-        self.LEFT  = "LEFT"
-        self.ALL   = "ALL"
+        self.FRONT = 'FRONT'
+        self.BACK = 'BACK'
+        self.RIGHT = 'RIGHT'
+        self.LEFT = 'LEFT'
+        self.ALL = 'ALL'
 
     def init_us(self):
         GPIO.setmode(GPIO.BCM)
@@ -36,69 +30,42 @@ class Ultrasonic_Sensors():
         GPIO.setup(self.GPIO_ECHO_B, GPIO.IN)
         GPIO.setup(self.GPIO_ECHO_R, GPIO.IN)
         GPIO.setup(self.GPIO_ECHO_L, GPIO.IN)
-        
 
     def distance(self, side):
-        # set Trigger to HIGH
         GPIO.output(self.GPIO_TRIGGER, True)
-        # # set Trigger after 0.01ms to LOW
-        time.sleep(0.00001)
+        time.sleep(1e-05)
         GPIO.output(self.GPIO_TRIGGER, False)
-    
         StartTime = time.time()
         StopTime = time.time()
-    
-        # save StartTime
         while GPIO.input(side) == 0:
             StartTime = time.time()
-    
-        # save time of arrival
+
         while GPIO.input(side) == 1:
             StopTime = time.time()
-    
-        # time difference between start and arrival
-        TimeElapsed = StopTime - StartTime
-        # multiply with the sonic speed (34300 cm/s)
-        # and divide by 2, because there and back
-        distance = TimeElapsed * 17150
 
-        if(distance > self.MAX_RANGE or distance < 0):
-            return self.MAX_RANGE 
+        TimeElapsed = StopTime - StartTime
+        distance = TimeElapsed * 17150
+        if distance > self.MAX_RANGE:
+            return self.MAX_RANGE
         else:
             return distance
-        return distance
-    
-    def measure_dist(self):  
-        self.f_us = self.distance(self.GPIO_ECHO_F)
-        self.b_us = self.distance(self.GPIO_ECHO_B)
-        self.l_us = self.distance(self.GPIO_ECHO_L)
-        self.r_us = self.distance(self.GPIO_ECHO_R)  
 
-        # return self.f_us,self.l_us,self.b_us,self.r_us
+    def measure_dist(self):
+        self.r_us = self.distance(self.GPIO_ECHO_R)
 
     def read_us(self):
         self.measure_dist()
-        front_us = round(self.f_us,self.ACCURACY)
-        back_us  = round(self.b_us,self.ACCURACY)
-        right_us = round(self.r_us,self.ACCURACY)
-        left_us  = round(self.l_us,self.ACCURACY)
-
-        # if(self.req_side == self.FRONT):
-        #     return front_us
-        # elif(self.req_side == self.BACK):
-        #     return back_us
-        # elif(self.req_side == self.RIGHT):
-        #     return right_us 
-        # elif(self.req_side == self.LEFT):
-        #     return left_us 
-        # elif(self.req_side == self.ALL):
-        #     return front_us,back_us,left_us,right_us
-        time.sleep(0.1)
-        return front_us,left_us,back_us,right_us
-
-if __name__ == '__main__':
-    run = Ultrasonic_Sensors()
-    while 1:
-        print(run.read_us())
-        # time.sleep(0.1)
-    
+        front_us = round(self.f_us, self.ACCURACY)
+        back_us = round(self.b_us, self.ACCURACY)
+        right_us = round(self.r_us, self.ACCURACY)
+        left_us = round(self.l_us, self.ACCURACY)
+        if self.req_side == self.FRONT:
+            return front_us
+        if self.req_side == self.BACK:
+            return back_us
+        if self.req_side == self.RIGHT:
+            return right_us
+        if self.req_side == self.LEFT:
+            return left_us
+        if self.req_side == self.ALL:
+            return (front_us, back_us, left_us, right_us)
---
+++ src/xbot_sensors/uss.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+from mybot_sdk.robot_setup import get_robot_cfg
+
+class Ultrasonic_Sensors():
+    def __init__(self,side="ALL"):
+        Ultrasonic = get_robot_cfg()["Sensors"]["Ultrasonic"]
+        self.GPIO_TRIGGER = Ultrasonic["Trigger"]
+        self.GPIO_ECHO_F = Ultrasonic["Front"]
+        self.GPIO_ECHO_B = Ultrasonic["Back"]
+        self.GPIO_ECHO_R = Ultrasonic["Right"]
+        self.GPIO_ECHO_L = Ultrasonic["Left"]
+        self.MIN_RANGE = Ultrasonic["Range"]["Min"]
+        self.MAX_RANGE = Ultrasonic["Range"]["Max"]
+
+        self.f_us = 0
+        self.b_us = 0
+        self.r_us = 0
+        self.l_us = 0
+
+        self.ACCURACY = 2 #Deciaml Places
+        self.init_us()
+
+        self.req_side = side
+        self.FRONT = "FRONT"
+        self.BACK  = "BACK"
+        self.RIGHT = "RIGHT"
+        self.LEFT  = "LEFT"
+        self.ALL   = "ALL"
+
+    def init_us(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO_F, GPIO.IN)
+        GPIO.setup(self.GPIO_ECHO_B, GPIO.IN)
+        GPIO.setup(self.GPIO_ECHO_R, GPIO.IN)
+        GPIO.setup(self.GPIO_ECHO_L, GPIO.IN)
+        
+    def cm_to_m(self,cm):
+        m = cm /100
+        return m
+
+    def distance(self, side):
+        # set Trigger to HIGH
+        GPIO.output(self.GPIO_TRIGGER, True)
+        # # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(self.GPIO_TRIGGER, False)
+    
+        StartTime = time.time()
+        StopTime = time.time()
+    
+        # save StartTime
+        while GPIO.input(side) == 0:
+            StartTime = time.time()
+    
+        # save time of arrival
+        while GPIO.input(side) == 1:
+            StopTime = time.time()
+    
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = TimeElapsed * 17150
+        distance = self.cm_to_m(distance)
+
+        if(distance > self.MAX_RANGE or distance < 0):
+            return self.MAX_RANGE 
+        elif distance < self.MIN_RANGE:
+            return self.MIN_RANGE
+        else:
+            return distance
+        return distance
+    
+    def measure_dist(self):  
+        self.f_us = self.distance(self.GPIO_ECHO_F)
+        self.b_us = self.distance(self.GPIO_ECHO_B)
+        self.l_us = self.distance(self.GPIO_ECHO_L)
+        self.r_us = self.distance(self.GPIO_ECHO_R)  
+
+        # return self.f_us,self.l_us,self.b_us,self.r_us
+
+    def read_us(self):
+        self.measure_dist()
+        front_us = round(self.f_us,self.ACCURACY)
+        back_us  = round(self.b_us,self.ACCURACY)
+        right_us = round(self.r_us,self.ACCURACY)
+        left_us  = round(self.l_us,self.ACCURACY)
+
+        # if(self.req_side == self.FRONT):
+        #     return front_us
+        # elif(self.req_side == self.BACK):
+        #     return back_us
+        # elif(self.req_side == self.RIGHT):
+        #     return right_us 
+        # elif(self.req_side == self.LEFT):
+        #     return left_us 
+        # elif(self.req_side == self.ALL):
+        #     return front_us,back_us,left_us,right_us
+        # time.sleep(0.1)
+        return front_us,left_us,back_us,right_us
+
+if __name__ == '__main__':
+    run = Ultrasonic_Sensors()
+    while 1:
+        print(run.read_us())
+        time.sleep(0.1)
+    
